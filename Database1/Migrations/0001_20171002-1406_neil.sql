40b56fea076d05e58dca6ef3d6ee589ea4d23309
Neil <neil@openprojects.co.uk>
2017-10-02 14:21:37 +0100

    Initial commit

---
+++ Database1/Migrations/0001_20171002-1406_neil.sql
@@ -0,0 +1,24 @@
+﻿-- <Migration ID="777cfa25-c3d8-49a1-b356-c874d27cdbea" />
+GO
+
+PRINT N'Creating [dbo].[Title]...';
+
+
+GO
+CREATE TABLE [dbo].[Title] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [Name]     NVARCHAR (50) NOT NULL,
+    [Rating]   NCHAR (3)     NOT NULL,
+    [Director] INT           NOT NULL,
+    CONSTRAINT [PK_Title] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+PRINT N'Creating unnamed constraint on [dbo].[Title]...';
+
+
+GO
+ALTER TABLE [dbo].[Title]
+    ADD CONSTRAINT [DF_Title_Rating] DEFAULT 'PG' FOR [Rating];
+
---
+++ Database1/Migrations/0002_20171002-1407_neil.sql
@@ -0,0 +1,25 @@
+﻿-- <Migration ID="6adf2754-5e78-4892-921a-8b13b5715214" />
+GO
+
+PRINT N'Creating [dbo].[Director]...';
+
+
+GO
+CREATE TABLE [dbo].[Director] (
+    [Id]   INT           IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Director] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+PRINT N'Creating [dbo].[Title->Director Constraint]...';
+
+
+GO
+ALTER TABLE [Title] 
+ADD CONSTRAINT [FK_Director]
+FOREIGN KEY([Director]) REFERENCES [Director]([Id]);
+
+GO
+
